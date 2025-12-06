6ea7e4b7cfbbc3e8519276ca3f112f8bb8893ea0
NishaMakone <makonenisha@gmail.com>
2025-12-06 12:46:21 +0530

    Hibernate ORM,JPA Entities and Tables in Spring Data JPA

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+Insert INTO practice.product_table (price, quantity, sku, title_x)
+values
+(12.4, 4, 'parle734', 'Parle Biscuit'),
+(14.4, 1, 'pepsi123', 'Pepsi'),
+(34.4, 10, 'Coke124', 'Coca Cola'),
+(16.4, 3, 'pepsi156', 'Mazza');
